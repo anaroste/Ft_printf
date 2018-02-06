392aada52ffe455878faae48cf31e751f404630f
Antoine Arostegui <anaroste@student.42.fr>
2018-02-06 15:27:06 +0100

    06/02 update mo x

--- printf_functions/ft_convert_mo.c
+++ printf_functions/ft_convert_mo.c
@@ -1,24 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_convert_x.c                                     :+:      :+:    :+:   */
+/*   ft_convert_mo.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anaroste <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/01/23 12:02:44 by anaroste          #+#    #+#             */
-/*   Updated: 2018/01/25 17:46:13 by anaroste         ###   ########.fr       */
+/*   Created: 2018/01/23 18:36:12 by anaroste          #+#    #+#             */
+/*   Updated: 2018/01/25 17:43:45 by anaroste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/libprintf.h"
 
-void	ft_convert_x(t_stock *stock)
+void		ft_convert_mo(t_stock *stock, va_list ap)
 {
-	stock->str = ft_custom_itoa_base(stock->ull, 16, stock->type);
-	if (ft_strlen(stock->str) < stock->opt[6])
-		ft_add_precision(stock);
-	else if (stock->opt[0] != 0)
-		ft_add_x(stock);
-	if (stock->opt[5] != 0)
-		ft_add_lenght(stock);
+	
 }
