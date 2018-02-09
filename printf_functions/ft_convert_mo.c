db81e5d6b4823d71899e7df36a4520b0e9bd1373
Antoine AROSTEGUI <anaroste@e1r11p23.42.fr>
2018-02-09 13:49:41 +0100

    09/02 add mo

--- printf_functions/ft_convert_mo.c
+++ printf_functions/ft_convert_mo.c
@@ -6,7 +6,7 @@
 /*   By: anaroste <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/23 18:36:12 by anaroste          #+#    #+#             */
-/*   Updated: 2018/01/25 17:43:45 by anaroste         ###   ########.fr       */
+/*   Updated: 2018/02/09 13:35:10 by anaroste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 void		ft_convert_mo(t_stock *stock, va_list ap)
 {
-	
+	stock->type = 'o';
+	stock->opt[7] = 3;
+	ft_convert_oux(stock, ap);
 }
