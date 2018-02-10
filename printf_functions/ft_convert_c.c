58a6d2112da284139b403d3e103946e56f1b834f
Antoine AROSTEGUI <anaroste@e2r9p19.42.fr>
2018-02-10 10:34:08 +0100

    10/02 update c

--- printf_functions/ft_convert_c.c
+++ printf_functions/ft_convert_c.c
@@ -6,7 +6,7 @@
 /*   By: anaroste <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/24 16:08:27 by anaroste          #+#    #+#             */
-/*   Updated: 2018/01/25 17:41:44 by anaroste         ###   ########.fr       */
+/*   Updated: 2018/02/10 10:33:20 by anaroste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,32 @@ void		ft_convert_c(t_stock *stock, va_list ap)
 {
 	char	*tmp;
 
-	tmp = (char *)malloc(sizeof(char) * 2);
-	 if (stock->opt[7] != 0)
-		 ft_convert_mc(stock, ap);
-	 tmp[0] = va_arg(ap, int);
-	 tmp[1] = '\0';
-	 stock->str = tmp;
-	 free(tmp);
-	 stock->opt[1] = 0;
-	 ft_add_lenght(stock);
+	if (stock->opt[7] != 0)
+		ft_convert_mc(stock, ap);
+	else
+	{
+		tmp = (char *)malloc(sizeof(char) * 2);
+		tmp[0] = va_arg(ap, int);
+		tmp[1] = '\0';
+		stock->str = tmp;
+		free(tmp);
+		stock->opt[1] = 0;
+		ft_add_lenght(stock);
+	}
 }
+/*
+int		main()
+{
+	t_stock		stock;
+	int			i;
+
+	i = -1;
+	stock.ull = 0;
+	stock.str = NULL;
+	stock.type = 'c';
+	while (++i < 9)
+		stock.opt[i++] = 0;
+
+
+}
+*/
